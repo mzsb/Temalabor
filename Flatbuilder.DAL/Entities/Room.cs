af03fc22fe31ab4bb28b976476c499ebeb4c077c
wolfi95 <szalkaititi@gmail.com>
2018-10-03 18:33:17 +0200

    valami elbaszodott de mar remelem jo

---
+++ Flatbuilder.DAL/Entities/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flatbuilder.DAL.Entities
+{
+    //generalization of all the rooms
+    public class Room
+    {
+        public int Id { get; set; }       
+        public double Price { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null;
+    }
+}
--- Flatbuilder.DTO/Costumer.cs
+++ Flatbuilder.DTO/Costumer.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; } = null;
     }
 }
