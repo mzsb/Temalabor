c5f4e772194d86ad1079558715bbba550e62ee72
wolfi95 <szalkaititi@gmail.com>
2018-10-03 22:18:33 +0200

    OrderItem
    foggalmam sincs kelleni fog e vagy nem

---
+++ Flatbuilder.DTO/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flatbuilder.DTO
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public string Note { get; set; }
+    }
+}
