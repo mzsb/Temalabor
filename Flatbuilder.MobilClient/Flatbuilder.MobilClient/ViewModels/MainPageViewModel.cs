bdd6331197aa24442223f26a2f5ec3f4591ad752
wolfi95 <szalkaititi@gmail.com>
2018-10-20 16:59:58 +0200

    mvvm

---
+++ Flatbuilder.MobilClient/Flatbuilder.MobilClient/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Flatbuilder.MobilClient.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Str { get; set; }
+    }
+}
