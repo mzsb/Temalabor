264ffda1534457c045649f9e685af165d5e1343c
wolfi95 <szalkaititi@gmail.com>
2018-10-25 20:58:53 +0200

    page navigation

--- Flatbuilder.MobilClient/Flatbuilder.MobilClient/ViewModels/LoginPageViewModel.cs
+++ Flatbuilder.MobilClient/Flatbuilder.MobilClient/ViewModels/LoginPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Flatbuilder.DTO;
+﻿using Fb.MC.Views;
+using Flatbuilder.DTO;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -6,11 +7,24 @@ using System.ComponentModel;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Flatbuilder.MobilClient.ViewModels
 {
     public class LoginPageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand LoginCommand { private set; get; }
+
+        public LoginPageViewModel()
+        {
+            LoginCommand = new Command(
+                async () =>
+                {
+                   
+                    await Application.Current.MainPage.Navigation.PushModalAsync(new MainPage());
+                });
+        }
     }
 }
