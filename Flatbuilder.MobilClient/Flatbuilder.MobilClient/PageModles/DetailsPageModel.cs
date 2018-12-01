a3491899aeab07288941087c15d6e618818ef01d
wolfi95 <szalkaititi@gmail.com>
2018-12-01 20:39:39 +0100

    delete order

--- Flatbuilder.MobilClient/Flatbuilder.MobilClient/PageModles/DetailsPageModel.cs
+++ Flatbuilder.MobilClient/Flatbuilder.MobilClient/PageModles/DetailsPageModel.cs
@@ -2,12 +2,19 @@
 using FreshMvvm;
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Fb.MC.Views
 {
     class DetailsPageModel : FreshBasePageModel
     {
+        static readonly Uri baseAddress = new Uri("http://10.0.2.2:51502/");
+
         private Order order;
         public Order Order
         {
@@ -21,6 +28,7 @@ namespace Fb.MC.Views
                 RaisePropertyChanged("Order");
             }
         }
+        ICommand DeleteOrderCommand { get; }
 
         public override void Init(object initData)
         {
@@ -29,6 +37,27 @@ namespace Fb.MC.Views
             foreach(Room r in Order.Rooms)
             {
                 r.Type = r.GetType();
+            }
+        }
+        public DetailsPageModel()
+        {
+            DeleteOrderCommand = new Command(
+                async () => 
+                {
+                    await DeleteOrder(Order.Id);
+                });
+        }
+
+        private async Task DeleteOrder(int id)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = baseAddress;
+                var response = await client.GetAsync("api/Order/delete/" + id);
+                if(response.StatusCode == HttpStatusCode.OK)
+                {
+                    await CoreMethods.PopToRoot(false);
+                }
             }
         }
     }
